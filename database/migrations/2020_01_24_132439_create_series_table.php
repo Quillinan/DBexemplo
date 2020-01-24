fc5857b4e715382a98d99cf61d2287571d4db9a8
Quillinan <milton.quillinan1798@gmail.com>
2020-01-24 13:19:29 -0300

    DBexemplo

---
+++ database/migrations/2020_01_24_132439_create_series_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('series', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('sinopse');
+            $table->integer('nota')->unsigned();
+            $table->unsignedbigInteger('user_id')->nullable();
+            $table->timestamps();
+        });
+
+
+        //Foreign Keys
+        Schema::table('series', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('series');
+    }
+}
